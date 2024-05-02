f9f8ed14fe0388f1493cb81a9a7412ab3df9bf4e
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-05-02 21:46:02 +0000

    Understanding JS

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     return a + b
+// }
+// let sum = addTwoNumbers(3 , 5)
+// console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) =>{
+    return a + b
+}
+let sum = addTwoNumbers(2, 3)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); Also valid
+const addTwoNumbers2 = (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hi there!");
+
+const sayHello = () => console.log('Hey');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p> 
+    This is a multiline String!
+    </p>`
+);
+console.log(returnMultipleLines());
